9173602ffaba6794a070ac4c0531a1eb5dc2737f
jeeweb <502079461@qq.com>
2017-07-11 23:09:41 +0800

    页面更新

--- src/main/java/cn/jeeweb/modules/demo/controller/FormDemoController.java
+++ src/main/java/cn/jeeweb/modules/demo/controller/FormDemoController.java
@@ -1,9 +1,6 @@
 package cn.jeeweb.modules.demo.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import cn.jeeweb.core.common.controller.BaseController;
-import cn.jeeweb.core.common.hibernate.dynamic.builder.DynamicHibernateStatementBuilder;
-import cn.jeeweb.core.common.hibernate.dynamic.builder.NoneDynamicHibernateStatementBuilder;
-import cn.jeeweb.core.utils.SpringContextHolder;
 import cn.jeeweb.modules.sys.service.IUserService;
 
 /**
